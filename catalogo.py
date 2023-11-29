48860ffa036334b8a17de708f398a0d7de73316b
Caue Trajano <caue.sw@gmail.com>
2023-11-28 23:52:20 -0300

    Criando objetos

---
+++ catalogo.py
@@ -0,0 +1,9 @@
+from BinarySearchTree import BinarySearchTree
+from filme import filme
+
+arv = BinarySearchTree()
+arv.add(filme("Carros", 10))
+arv.add(filme("Chaves em acapuco", 99))
+arv.add("V de vendeta", 66)
+arv.inorder()
+
---
+++ filme.py
@@ -0,0 +1,29 @@
+class filme:
+    def __init__(self, title, price):
+        self.titulo = title
+        self.preco = price
+        self.estado = True
+
+    # @property
+    # def titulo(self):
+    #     return self.__titulo
+
+    # @property
+    # def preco(self):
+    #     return self.__preco
+    
+    # @preco.setter
+    # def preco(self, valor):
+    #     self.__preco = valor
+    
+    # @titulo.setter
+    # def titulo(self, newTitle):
+    #     self.__titulo = newTitle
+
+    # @property
+    # def estado(self):
+    #     return self.__estado
+    
+    # @estado.setter
+    # def estado(self, estado):
+    #     self.__estado = estado
