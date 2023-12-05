2ab3b5ff3d166a2ad01fc34567975b1fe86d14be
steph4nn <laurostephan@gmail.com>
2023-12-05 09:40:15 -0300

    REFACTOR: pequenos ajustes

--- Objects/catalogo.py
+++ Objects/catalogo.py
@@ -1,15 +1,20 @@
 import os
 import sys
-root_dir = os.path.dirname(os.path.abspath(__file__))
+import json
+
+
+#Caminho para o diretório raiz
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, root_dir)
+
+#Importando as classes necessárias
 from Structures.binarySearchTree import BinarySearchTree
 from Objects.filme import Filme
-import json
+
 
 arv = BinarySearchTree()
 
-with open("../Data/filmes.json", encoding="utf-8") as filmes:
+with open("Data/filmes.json", encoding="utf-8") as filmes:
     lista_de_filmes = json.load(filmes)
 
 for filme in lista_de_filmes:
@@ -23,5 +28,5 @@ def showCat():
         nome = nome.split("|")
         print(nome[0])
     #return arr
-showCat()
+
 
