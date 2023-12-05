1d55e104a5be8255577f9bff94ab885a0ac14f3e
Caue Trajano <caue.sw@gmail.com>
2023-12-05 10:47:57 -0300

    def alugar

--- Objects/catalogo.py
+++ Objects/catalogo.py
@@ -2,7 +2,6 @@ import os
 import sys
 import json
 
-
 #Caminho para o diretório raiz
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, root_dir)
@@ -20,6 +19,7 @@ with open("Data/filmes.json", encoding="utf-8") as filmes:
 for filme in lista_de_filmes:
     arv.add(Filme(filme['nome'],filme['preco']))
 
+
 def showCat():
     arr = []
     arr = arv.returnAll()
@@ -30,4 +30,18 @@ def showCat():
     #     print(nome[0])
     return arr
 
-showCat()
+if(arv.search(Filme("Cidadão Kane",0)) is not None):
+    print("Filme Disponivel")
+    nome = str(arv.search(Filme("Cidadão Kane",0)))
+    nome = nome.split("|")
+    print(nome[2])
+    if(nome[2]=="disponivel"):
+        #funçao que aluga
+        pass
+    else:
+        #return "Não foi possivel"
+        pass
+else:
+    print("Filme indisponivel")
+
+
